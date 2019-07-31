64ed705381551333e0718a9f76b75f11eb3669b2
Forve <tobias.forve@gmx.de>
2019-07-31 14:23:31 +0200

    Save

---
+++ HordeSimulator_Unity/Assets/Scripts/HeroAI/HeroAI_Controller.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class HeroAI_Controller : MonoBehaviour
+{
+    /* To Do
+    *   Fire, run, Reload, Heal, Suicide
+    */
+    [Header("Testing only")]
+    public Vector3 destination;     
+            
+    [Header("Objects")]
+    public GameObject targetObject;                                 //Object to Target (enemy to shoot, ammo to get, health to collect)
+
+    [Header("Scoring System")]
+    [Range(0,1)][SerializeField]    private float score;            // score calculated to choose action
+    [SerializeField]                private bool veto = false;      // if true action can not be executed, if true utility = 0
+
+    private NavMeshAgent agent;
+
+    // ---------------------------------------------
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        destination = targetObject.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        agent.SetDestination(destination);
+    }
+}
