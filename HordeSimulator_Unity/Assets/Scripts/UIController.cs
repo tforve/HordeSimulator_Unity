76b76282d837ed34e18f88bbb472be0dd1c1a2e7
Forve <tobias.forve@gmx.de>
2019-08-07 21:40:33 +0200

    up

--- HordeSimulator_Unity/Assets/Scripts/UIController.cs
+++ HordeSimulator_Unity/Assets/Scripts/UIController.cs
@@ -5,6 +5,20 @@ using UnityEngine.UI;
 
 public class UIController : MonoBehaviour
 {
+    private static UIController _instance;
+    public static UIController MyInstance
+    {
+        get
+        {
+            if (_instance == null)
+            {
+                _instance = GameObject.FindObjectOfType<UIController>();
+            }
+            return _instance;
+        }
+    }
+
+
     [SerializeField] private Text heighestTxt, evadeTxt, healthTxt, manaTxt, shootTxt;                         // for debuging purpose
     [SerializeField] private AI_EvadeEnemy MyAi_Evade;
     [SerializeField] private AI_SeekHealth MyAi_SeekHeal;
@@ -12,27 +26,26 @@ public class UIController : MonoBehaviour
     [SerializeField] private AI_ShootEnemy MyAi_Shoot;
 
     private float maxWeight;
-    
+
     [SerializeField] private Text killCountTxt;
     private int killCount = 0;
-    [SerializeField] private Slider actualAiState;
-    
-    // Start is called before the first frame update
-    void Start()
-    {
-
-    }
+    public Slider actualAiState;
 
     // Update is called once per frame
     void Update()
     {
-        if(HeroAI_Controller.MyInstance != null)
-        maxWeight = HeroAI_Controller.MyInstance.MyMaxWeight;
+        if (HeroAI_Controller.MyInstance != null)
+            maxWeight = HeroAI_Controller.MyInstance.MyMaxWeight;
         //UI Debug Only
         heighestTxt.text = "Highest Weight: " + maxWeight;
         evadeTxt.text = "Evade: " + MyAi_Evade.MyWeight;
         healthTxt.text = "Heal: " + MyAi_SeekHeal.MyWeight;
         manaTxt.text = "Mana: " + MyAi_SeekMana.MyWeight;
         shootTxt.text = "Fight: " + MyAi_Shoot.MyWeight;
+    }
+
+    public void SetSliderValue(float value)
+    {
+        actualAiState.value = value;
     }
 }
